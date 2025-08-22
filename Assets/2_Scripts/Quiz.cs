6e3d0d50e78622f7d50fc2398ab50ede8d2ea9b7
이도헌 <vahyeon2516@sonline20.sen.go.kr>
2025-08-22 11:39:41 +0900

    123124

--- Assets/2_Scripts/Quiz.cs
+++ Assets/2_Scripts/Quiz.cs
@@ -1,18 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
-    [SerializeField] TextMeshProUGUI[] answerTextArr;
+    //[SerializeField] TextMeshProUGUI[] answerTextArr;
+    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite defaltAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questionText.text = question.GetQuestion();
 
-        for (int i = 0; i < answerTextArr.Length; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            answerTextArr[i].text = question.GetAnswer(i);
+            answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetAnswer(i);
+            //answerTextArr[i].text = question.GetAnswer(i);
         }
     }
+
+    public void OnAnswerButtonClick(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "정답입니다!";
+            answerButtons[index].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
+    }   
 }
